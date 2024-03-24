f77430c0856a5fbb70ecc5542080f4dd822304aa
Oscar Lee <oscarlee8787@gmail.com>
2024-03-24 15:39:36 +0800

    created ballot.sol structure

--- test/HelloWorld.ts
+++ test/HelloWorld.ts
@@ -1,23 +1,32 @@
 import { expect } from "chai";
 import { viem } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+
 
 describe('HelloWorld', function (){
+    async function deployContractFixture() {
+      const publicClient = await viem.getPublicClient();
+      const [owner, otherAccount] = await viem.getWalletClients();
+      const helloWorldContract = await viem.deployContract("HelloWorld");
+      return {
+        publicClient,
+        owner,
+        otherAccount,
+        helloWorldContract,
+      };
+    }
+
   it ("Shoud give a Hello World", async function () {
-    const publicClient = await viem.getPublicClient();
-    const [owner, otherAccount] = await viem.getWalletClients();
-    const helloWorldContract = await viem.deployContract("HelloWorld");
+    const {helloWorldContract} = await loadFixture(deployContractFixture);
     const helloWorldText = await helloWorldContract.read.helloWorld();
     expect(helloWorldText).to.equal("Hello World!");
   });
 
   it ("Shoud change text correctly", async function () {
-    const helloWorldContract = await viem.deployContract("HelloWorld");
-    const helloWorldText = await helloWorldContract.read.helloWorld();
+    const {helloWorldContract, publicClient} = await loadFixture(deployContractFixture);
     const tx = await helloWorldContract.write.setText(["Potato!"]);
-    const publicClient = await viem.getPublicClient();
-    const receipt = await publicClient.getTransactionReceipt( {hash: tx} );
     const helloWorldText2 = await helloWorldContract.read.helloWorld();
+    const receipt = await publicClient.getTransactionReceipt( {hash: tx} );
     expect(helloWorldText2).to.equal("Potato!");
   });
-
 });
